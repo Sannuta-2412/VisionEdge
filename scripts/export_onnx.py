f061ac1c17dbabc5b6b81d8102adbacfa7e863c4
SATHISH LODIGA <lodigasatish@gmail.com>
2026-07-29 21:54:22 +0530

    Delete scripts directory

--- scripts/export_onnx.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load pretrained YOLO model
-model = YOLO("yolov8n.pt")
-
-# Export to ONNX
-model.export(format="onnx")
-
-print("YOLO model exported to ONNX successfully!")
